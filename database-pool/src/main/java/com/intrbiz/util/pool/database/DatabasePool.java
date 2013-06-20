86028e3a43c7965263e85b985f717a5ba6e17953
Chris Ellis <chris@intrbiz.com>
2013-06-20 15:07:31 +0100

    Added some util methods which make it simple to create a database pool

--- database-pool/src/main/java/com/intrbiz/util/pool/database/DatabasePool.java
+++ database-pool/src/main/java/com/intrbiz/util/pool/database/DatabasePool.java
@@ -9,4 +9,54 @@ public interface DatabasePool extends Configurable<DatabasePoolConfiguration>
     Connection connect() throws Exception;
     
     void close();
+    
+    
+    public static final class Default
+    {
+        public static DatabasePool create(String url, String username, String password) throws Exception
+        {
+            DatabasePoolConfiguration cfg = new DatabasePoolConfiguration();
+            cfg.setUrl(url);
+            cfg.setUsername(username);
+            cfg.setPassword(password);
+            DatabasePool pool = new DBCPPool();
+            pool.configure(cfg);
+            return pool;
+        }
+        
+        public static DatabasePool create(String url, String username, String password, String validationSql) throws Exception
+        {
+            DatabasePoolConfiguration cfg = new DatabasePoolConfiguration();
+            cfg.setUrl(url);
+            cfg.setUsername(username);
+            cfg.setPassword(password);
+            cfg.setValidationSql(validationSql);
+            DatabasePool pool = new DBCPPool();
+            pool.configure(cfg);
+            return pool;
+        }
+        
+        public static DatabasePool create(Class<?> driver, String url, String username, String password, String validationSql) throws Exception
+        {
+            DatabasePoolConfiguration cfg = new DatabasePoolConfiguration();
+            cfg.setUrl(url);
+            cfg.setUsername(username);
+            cfg.setPassword(password);
+            cfg.setValidationSql(validationSql);
+            DatabasePool pool = new DBCPPool();
+            pool.configure(cfg);
+            return pool;
+        }
+        
+        public static DatabasePool create(Class<?> driver, String url, String username, String password) throws Exception
+        {
+            DatabasePoolConfiguration cfg = new DatabasePoolConfiguration();
+            cfg.setUrl(url);
+            cfg.setUsername(username);
+            cfg.setPassword(password);
+            DatabasePool pool = new DBCPPool();
+            pool.configure(cfg);
+            return pool;
+        }
+    }
 }
