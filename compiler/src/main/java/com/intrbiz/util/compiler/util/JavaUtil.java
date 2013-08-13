05aac1ebcef91455fd799a5f45d69caac704cbdc
Chris Ellis <chris@intrbiz.com>
2013-08-13 20:54:31 +0100

    Some Java code utility methods

--- compiler/src/main/java/com/intrbiz/util/compiler/util/JavaUtil.java
+++ compiler/src/main/java/com/intrbiz/util/compiler/util/JavaUtil.java
@@ -1,5 +1,7 @@
 package com.intrbiz.util.compiler.util;
 
+import java.lang.reflect.Field;
+
 public class JavaUtil
 {
     public static boolean isJavaPrimitive(String type)
@@ -12,5 +14,31 @@ public class JavaUtil
                "short".equals(type) ||
                "byte".equals(type) ||
                "character".equals(type);
+    }
+    
+    public static String escapeString(String str)
+    {
+        return str.replace("\"", "\\\"");
+    }
+    
+    public static String lcFirst(String str)
+    {
+        return str.substring(0, 1).toLowerCase() + str.substring(1);
+    }
+    
+    public static String ucFirst(String str)
+    {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+    
+    public static String getterName(Field field)
+    {
+        if (boolean.class == field.getType() || Boolean.class == field.getType()) return "is" + ucFirst(field.getName());    
+        return "get" + ucFirst(field.getName());
+    }
+    
+    public static String setterName(Field field)
+    {
+        return "set" + ucFirst(field.getName());
     }
 }
