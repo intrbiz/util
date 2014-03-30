7a8340185e441d63f0962965f8588ab1271730cc
Chris Ellis <chris@intrbiz.com>
2014-03-30 17:53:33 +0100

    Added hashcode and equals methods

--- queue/src/main/java/com/intrbiz/queue/name/RoutingKey.java
+++ queue/src/main/java/com/intrbiz/queue/name/RoutingKey.java
@@ -8,4 +8,19 @@ public abstract class RoutingKey
     {
         return this.routingKey();
     }
+    
+    public final int hashCode()
+    {
+        return this.routingKey().hashCode();
+    }
+    
+    public final boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o instanceof RoutingKey)
+        {
+            return this.routingKey().equals(((RoutingKey) o).routingKey());
+        }
+        return false;
+    }
 }
