bbf4bca5f46e654b51ba515af5eef787a6593b0c
Chris Ellis <chris@intrbiz.com>
2014-07-02 23:35:00 +0100

    Add remove binding and allow binding by type keys

--- queue/src/main/java/com/intrbiz/queue/Consumer.java
+++ queue/src/main/java/com/intrbiz/queue/Consumer.java
@@ -1,5 +1,7 @@
 package com.intrbiz.queue;
 
+import com.intrbiz.queue.name.RoutingKey;
+
 
 
 
@@ -10,6 +12,18 @@ public interface Consumer<T> extends AutoCloseable
     DeliveryHandler<T> handler();
     
     void addBinding(String binding);
+    
+    default void addBinding(RoutingKey key)
+    {
+        this.addBinding(key.toString());
+    }
+    
+    void removeBinding(String binding);
+    
+    default void removeBinding(RoutingKey key)
+    {
+        this.removeBinding(key.toString());
+    }
     
     @Override
     void close();
