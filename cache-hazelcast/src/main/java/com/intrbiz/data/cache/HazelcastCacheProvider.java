e7e8d695d6237e7bac2c39be2a49f9eaef7effa2
Chris Ellis <chris@intrbiz.com>
2015-01-13 19:30:47 +0000

    Tweak default Hazelcast config

--- cache-hazelcast/src/main/java/com/intrbiz/data/cache/HazelcastCacheProvider.java
+++ cache-hazelcast/src/main/java/com/intrbiz/data/cache/HazelcastCacheProvider.java
@@ -1,8 +1,10 @@
 package com.intrbiz.data.cache;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.MapConfig.EvictionPolicy;
+import com.hazelcast.config.NearCacheConfig;
 import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
@@ -48,9 +50,22 @@ public class HazelcastCacheProvider implements CacheProvider
                     // add update configuration for our maps
                     MapConfig cacheMapConfig = config.getMapConfig("intrbiz.cache.*");
                     // add default config for cache maps
-                    cacheMapConfig.setMaxIdleSeconds(3600);
+                    cacheMapConfig.setMaxIdleSeconds(1 * 60 * 60); /* Objects are removed if they are idle for 1 hour */
                     cacheMapConfig.setEvictionPolicy(EvictionPolicy.LRU);
                     cacheMapConfig.setEvictionPercentage(25);
+                    cacheMapConfig.setTimeToLiveSeconds(12 * 60 * 60); /* Objects are always refreshed every 12 hours */
+                    cacheMapConfig.setBackupCount(0); /* We're a cache we don't care if we need to visit the backing store */
+                    cacheMapConfig.setAsyncBackupCount(0); /* We're a cache we don't care if we need to visit the backing store */
+                    cacheMapConfig.setInMemoryFormat(InMemoryFormat.OBJECT); /* Store the objects in object form, we want retrival to be fast and consitent */
+                    // setup nearline cache
+                    NearCacheConfig cacheMapNLConfig = new NearCacheConfig();
+                    cacheMapNLConfig.setCacheLocalEntries(false);
+                    cacheMapNLConfig.setEvictionPolicy("LRU");
+                    cacheMapNLConfig.setInMemoryFormat(InMemoryFormat.OBJECT);
+                    cacheMapNLConfig.setInvalidateOnChange(true);
+                    cacheMapNLConfig.setMaxIdleSeconds(10 * 60); /* 10 minute idle time */
+                    cacheMapNLConfig.setTimeToLiveSeconds(1 * 60 * 60); /* 1 hour TTL */
+                    cacheMapConfig.setNearCacheConfig(cacheMapNLConfig);
                     config.addMapConfig(cacheMapConfig);
                 }
             }
