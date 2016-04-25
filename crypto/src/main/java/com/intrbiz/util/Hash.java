beba4de831ef30fc8f864eef13c0c6fd1e423b29
Chris Ellis <chris@intrbiz.com>
2016-04-25 05:25:21 +0100

    Add SHA256-HMAC and BufferSlices

--- crypto/src/main/java/com/intrbiz/util/Hash.java
+++ crypto/src/main/java/com/intrbiz/util/Hash.java
@@ -13,6 +13,109 @@ public class Hash
     public static final String SHA256 = "SHA-256";
     public static final String SHA512 = "SHA-512";
     
+    public static final int SHA256_LENGTH = 32;
+    
+    private static final byte HMAC_OPAD = 0x5C;
+    private static final byte HMAC_IPAD = 0x36;
+    
+    public static final byte[] sha256HMAC(byte[] key, BufferSlice... slices)
+    {
+        // sort out the key
+        if (key.length > SHA256_LENGTH)
+        {
+            key = sha256(key);
+        }
+        if (key.length < SHA256_LENGTH)
+        {
+            byte[] tmp = new byte[SHA256_LENGTH];
+            System.arraycopy(key, 0, tmp, 0, key.length);
+            key = tmp;
+        }
+        return sha256(xor(key, HMAC_OPAD), sha256Keyed(xor(key, HMAC_IPAD), slices));
+
+    }
+    
+    private static final byte[] sha256Keyed(byte[] key, BufferSlice... slices)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance(SHA256);
+            md.update(key);
+            for (BufferSlice slice : slices)
+            {
+                md.update(slice.buffer, slice.offset, slice.length);
+            }
+            return md.digest();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("No implementation for SHA-256", e);
+        }
+    }
+    
+    
+    public static final byte[] sha256HMAC(byte[] key, byte[]... data)
+    {
+        // sort out the key
+        if (key.length > SHA256_LENGTH)
+        {
+            key = sha256(key);
+        }
+        if (key.length < SHA256_LENGTH)
+        {
+            byte[] tmp = new byte[SHA256_LENGTH];
+            System.arraycopy(key, 0, tmp, 0, key.length);
+            key = tmp;
+        }
+        return sha256(xor(key, HMAC_OPAD), sha256Keyed(xor(key, HMAC_IPAD), data));
+
+    }
+    
+    private static final byte[] xor(byte[] data, byte val)
+    {
+        byte[] out = new byte[data.length];
+        for (int i = 0; i < data.length; i++)
+        {
+            out[i] = (byte) (data[i] ^ val);
+        }
+        return out;
+    }
+    
+    private static final byte[] sha256Keyed(byte[] key, byte[]... data)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance(SHA256);
+            md.update(key);
+            for (byte[] d : data)
+            {
+                md.update(d);
+            }
+            return md.digest();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("No implementation for SHA-256", e);
+        }
+    }
+    
+    public static final byte[] sha256(BufferSlice... slices)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance(SHA256);
+            for (BufferSlice slice : slices)
+            {
+                md.update(slice.buffer, slice.offset, slice.length);
+            }
+            return md.digest();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("No implementation for SHA-256", e);
+        }
+    }
+    
     public static final byte[] sha256(byte[]... data)
     {
         try
@@ -27,6 +130,23 @@ public class Hash
         catch (NoSuchAlgorithmException e)
         {
             throw new RuntimeException("No implementation for SHA-256", e);
+        }
+    }
+    
+    public static final byte[] sha512(BufferSlice... slices)
+    {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance(SHA512);
+            for (BufferSlice slice : slices)
+            {
+                md.update(slice.buffer, slice.offset, slice.length);
+            }
+            return md.digest();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("No implementation for SHA-512", e);
         }
     }
     
@@ -97,5 +217,30 @@ public class Hash
             sb.append(HEX_NIBBLE[b[i] & 0xF]);
         }
         return sb.toString();
+    }
+    
+    public static final class BufferSlice
+    {
+        public final byte[] buffer;
+        
+        public final int offset;
+        
+        public final int length;
+
+        public BufferSlice(byte[] buffer, int offset, int length)
+        {
+            super();
+            this.buffer = buffer;
+            this.offset = offset;
+            this.length = length;
+        }
+        
+        public BufferSlice(byte[] buffer)
+        {
+            super();
+            this.buffer = buffer;
+            this.offset = 0;
+            this.length = buffer.length;
+        }
     }
 }
